00aedac016e06dd0593eef52b60f1f3748362ca8
jorgelserve <jorgeluisserna@iMac-de-Jorge.local>
2016-11-09 07:16:29 -0500

    v.01

---
+++ midi/midi.ino
@@ -0,0 +1,111 @@
+// MPU-6050 Short Example Sketch
+// By Arduino User JohnChi
+// August 17, 2014
+// Public Domain
+#include<Wire.h>
+#include "notas.h"
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t x,y;
+
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.begin(115200);
+
+}
+void loop(){
+  
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  x=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  y=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+
+  int val = map(x,0,6000,0,8);
+  
+
+  int nota;
+  switch(val) {
+    
+    case 0:
+    nota=DO;
+    noteOn(0x90,nota,0x45);
+    delay(500);
+    noteOn(0x90,nota,0x00);
+    break;
+    
+    case 1:
+    nota=RE;
+    noteOn(0x90,nota,0x45);
+    delay(500);
+    noteOn(0x90,nota,0x00);
+    break;
+
+    case 2:
+    nota=MI;
+    noteOn(0x90,nota,0x45);
+    delay(500);
+    noteOn(0x90,nota,0x00);
+    break;
+
+    case 3:
+    nota=FA;
+    noteOn(0x90,nota,0x45);
+    delay(500);
+    noteOn(0x90,nota,0x00);
+    break;
+
+    case 4:
+    nota=SOL;
+    noteOn(0x90,nota,0x45);
+    delay(500);
+    noteOn(0x90,nota,0x00);
+    break;
+
+    case 5:
+    nota=LA;
+    noteOn(0x90,nota,0x45);
+    delay(500);
+    noteOn(0x90,nota,0x00);
+    break;
+    
+    case 6:
+    nota=SI;
+    noteOn(0x90,nota,0x45);
+    delay(500);
+    noteOn(0x90,nota,0x00);
+    break;
+
+    case 7:
+    nota=DO1;
+    noteOn(0x90,nota,0x45);
+    delay(500);
+    noteOn(0x90,nota,0x00);
+    break;
+
+    case 8:
+    nota=RE1;
+    noteOn(0x90,nota,0x45);
+    delay(500);
+    noteOn(0x90,nota,0x00);
+    break;
+
+    case 9:
+    nota=MI1;
+    noteOn(0x90,nota,0x45);
+    delay(500);
+    noteOn(0x90,nota,0x00);
+    break;
+  }
+  noteOn(0x00,nota,0x00);
+}
+
+void noteOn(int cmd, int pitch, int velocity) {
+    Serial.write(cmd);
+    Serial.write(pitch);
+    Serial.write(velocity);
+  }
